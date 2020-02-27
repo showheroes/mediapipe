095daa6a0b3f13ea0fb067c3bc29a35b8daba775
Patrick Jähnichen <patrick.jaehnichen@gmail.com>
2020-02-27 16:14:06 +0100

    remove reference to self

--- server/createhero/app.py
+++ server/createhero/app.py
@@ -32,7 +32,7 @@ class TaskExecutor(PeriodicCallback):
     def __init__(self, settings):
         self.q = settings['task_queue']
         self.d = settings['tasks']
-        self.data_dir = self.settings['working_directory']
+        self.data_dir = settings['working_directory']
         super().__init__(self._do, 1e3)
 
     async def _do(self):
