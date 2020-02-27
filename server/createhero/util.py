14595b9ea052bfe7d73c0e57d2f23bccb1deba9d
Patrick Jähnichen <patrick.jaehnichen@gmail.com>
2020-02-27 13:34:55 +0100

    update task process class

--- server/createhero/util.py
+++ server/createhero/util.py
@@ -37,23 +37,27 @@ class VideoReformatTask(object):
     FORMAT_16x9 = '16:9'
     FORMAT_9x16 = '9:16'
 
+    STATUS_SUBMITTED = 'submitted'
     STATUS_INIT = 'initialized'
     STATUS_RUNNING = 'running'
     STATUS_SUCCESS = 'success'
     STATUS_STOPPED = 'stopped'
 
-    def __init__(self, working_base_dir, format, logging = False):
+    def __init__(self, task_id, working_base_dir, task_data, logging = False):
         self.working_base_dir = working_base_dir
         self.logging = logging
         if self.logging:
             self.log_reader_queue = queue.Queue()
         # save format
-        self.format = format
+        self.format = task_data['target_format']
+        # keep track of data
+        self.task_data = task_data
+        self.task_data['progress'] = []
         # create task id
         self.task_id = str(uuid.uuid4())
         os.mkdir(os.path.join(self.working_base_dir, self.task_id))
-        self.status = self.STATUS_INIT
-        self.progress = []
+        self.task_data['status'] = self.STATUS_INIT
+        # self.progress = []
 
     def get_task_directory(self):
         return os.path.join(self.working_base_dir, self.task_id)
@@ -62,7 +66,9 @@ class VideoReformatTask(object):
         input_file_name, input_ext = os.path.splitext(input_file)
         output_file = input_file_name + '_' + self.format + input_ext
         self.input_file = os.path.join(self.get_task_directory(), input_file)
+        self.task_data['input_file'] = self.input_file
         self.output_file = os.path.join(self.get_task_directory(), output_file)
+        self.task_data['output_file'] = self.output_file
 
         # prepare call to subprocess
         self.command = ['bazel-bin/mediapipe/examples/desktop/autoflip/run_autoflip',
@@ -77,26 +83,28 @@ class VideoReformatTask(object):
         return self.output_file
 
     async def start(self):
-        if self.status != self.STATUS_INIT:
+        if self.task_data['status'] != self.STATUS_INIT:
             return "Task not yet initialized."
         # Launch the command as subprocess, route stderr to stdout
         if self.logging:
             self.process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             # Launch the asynchronous readers of the process' stdout and stderr.
             self.log_reader = AsynchronousFileReader(self.process.stdout, self.log_reader_queue)
-            self.log_reader.start()
+            self.log_reader.run()
         else:
             self.process = subprocess.Popen(self.command)
-        self.status = self.STATUS_RUNNING
-        self.is_finished()
+        self.task_data['status'] = self.STATUS_RUNNING
+        if self.logging:
+            while not self.is_finished() and not self.log_reader_queue.empty():
+                self.task_data['progress'].append(self.log_reader_queue.get())
 
-    def get_progress(self):
-        if not self.logging:
-            return ['']
-        # get the progress from the subprocess
-        while not self.log_reader_queue.empty():
-            self.progress.append(self.log_reader_queue.get())
-        return '\n'.join(progress)
+    # def get_progress(self):
+    #     if not self.logging:
+    #         return ['']
+    #     # get the progress from the subprocess
+    #     while not self.log_reader_queue.empty():
+    #         self.progress.append(self.log_reader_queue.get())
+    #     return '\n'.join(progress)
 
     def is_finished(self):
         status = self.process.poll()
@@ -108,8 +116,13 @@ class VideoReformatTask(object):
             process.stdout.close()
             process.stderr.close()
             if status == 0:
-                self.status = self.STATUS_SUCCESS
+                self.task_data['status'] = self.STATUS_SUCCESS
             else:
-                self.status = self.STATUS_STOPPED
+                self.task_data['status'] = self.STATUS_STOPPED
+            # put down the data into the directory for later inspection
+            with open(os.path.join(self.get_task_directory(), 'process_output.txt')) as f:
+                f.write('\n'.join(self.task_data['progress']))
+            with open(os.path.join(self.get_task_directory(), 'STATE')) as f:
+                f.write(self.status)
             return True
         return False
