5d3c8ce3c87ca45f5df500341677a25a422c3e7a
Patrick Jähnichen <patrick.jaehnichen@gmail.com>
2020-03-04 17:48:30 +0100

    replace placeholders inside command

--- server/createhero/util.py
+++ server/createhero/util.py
@@ -112,7 +112,7 @@ class VideoReformatTask(object):
         # prepare call to subprocess
         self.command = ['/mediapipe/bazel-bin/mediapipe/examples/desktop/autoflip/run_autoflip',
                         '--calculator_graph_config_file=/mediapipe/mediapipe/examples/desktop/autoflip/autoflip_graph.pbtxt',
-                        f'--input_side_packets=input_video_path={input_no_audio},output_video_path={output_no_audio},aspect_ratio={self.task_data["target_format"]}'
+                        f'--input_side_packets=input_video_path={self.task_data["input_file_no_audio"]},output_video_path={self.task_data["output_file_no_audio"]},aspect_ratio={self.task_data["target_format"]}'
                         ]
 
     async def start(self):
