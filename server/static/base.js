3f7d220ce0cd03ad7700dfb8d25ab99a7b7f8178
Patrick Jähnichen <patrick.jaehnichen@gmail.com>
2020-03-05 13:46:36 +0100

    js loading stuff

--- server/static/base.js
+++ server/static/base.js
@@ -1,30 +1,33 @@
-function initiateProgressSocket(textarea, taskID, deployPath) {
-	let host = getBaseURL();
-	console.log("got host: " + host);
-	let socket = new Websocket("wss://" + host + "/" + deployPath + "/video/flip/ui/tasks/ " + taskID + "/progress");
-	console.log("constructed websocket")
-	socket.onopen = function(e) {
-		console.log("initiated progress socket");
-		socket.send("progress");
-	};
+$(document).ready(function(){
+	function initiateProgressSocket(textarea, taskID, deployPath) {
+		let host = getBaseURL();
+		console.log("got host: " + host);
+		let socket = new Websocket("wss://" + host + "/" + deployPath + "/video/flip/ui/tasks/ " + taskID + "/progress");
+		console.log("constructed websocket")
+		socket.onopen = function(e) {
+			console.log("initiated progress socket");
+			socket.send("progress");
+		};
 
-	socket.onmessage = function(event) {
-		console.log("[Progress] " + event.data);
-		textarea.val(event.data);
-	};
+		socket.onmessage = function(event) {
+			console.log("[Progress] " + event.data);
+			textarea.val(event.data);
+		};
 
-	socket.onclose = function(event) {
-	  if (event.wasClean) {
-	    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-	  } else {
-	    // e.g. server process killed or network down
-	    // event.code is usually 1006 in this case
-	    alert('[close] Connection died');
-	  }
-	};
+		socket.onclose = function(event) {
+		  if (event.wasClean) {
+		    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+		  } else {
+		    // e.g. server process killed or network down
+		    // event.code is usually 1006 in this case
+		    alert('[close] Connection died');
+		  }
+		};
 
-}
+	}
 
-function getBaseURL() {
-	return window.location.host;
-}
+	function getBaseURL() {
+		return window.location.host;
+	}
+	
+});
