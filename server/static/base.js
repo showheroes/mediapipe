85c668f9c760d7f2fa7a0858b9423f64d943a8f2
Patrick Jähnichen <patrick.jaehnichen@gmail.com>
2020-03-06 09:25:21 +0100

    set interval on open, log to console

--- server/static/base.js
+++ server/static/base.js
@@ -3,7 +3,10 @@ function initiateProgressSocket(progressWindow, taskID, deployPath) {
 	let socketPath = "ws://" + host + "/" + deployPath + "/video/flip/ui/tasks/" + taskID + "/progress";
 	let socket = new WebSocket(socketPath);
 	socket.onopen = function(e) {
-		socket.send("progress");
+		window.setInterval(function(){
+			socket.send("progress");
+			console.log("[action] reloading progress");
+		}, 1500);
 	};
 
 	socket.onmessage = function(event) {
@@ -12,17 +15,13 @@ function initiateProgressSocket(progressWindow, taskID, deployPath) {
 
 	socket.onclose = function(event) {
 	  if (event.wasClean) {
-	    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+	    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
 	  } else {
 	    // e.g. server process killed or network down
 	    // event.code is usually 1006 in this case
-	    alert('[close] Connection died');
+	    console.log('[close] Connection died');
 	  }
 	};
-	window.setInterval(function(){
-		socket.send("progress");
-		console.log("reloading progress");
-	}, 1500);
 }
 
 function getBaseURL() {
